68433478a31d3a58555f7fcfa5865f90ab0f4017
Sarthak Kumar Yadav <sarthakyadavjnp108@gmail.com>
2026-03-02 14:12:56 +0530

    Add files via upload

---
+++ ATM-Interface/ATMinterface.java
@@ -0,0 +1,7 @@
+public class ATMinterface {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(1000); // Initial balance
+        ATM atm = new ATM(account);
+        atm.start();
+    }
+}
---
+++ ATM-Interface/BankAccount.java
@@ -0,0 +1,29 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount!");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient balance or invalid amount!");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
